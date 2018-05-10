19d058417dafd7780f0bccc1d2148007b127e30e
Matic Zagmajster <maticzagmajster@gmail.com>
2018-05-10 20:09:06 +0200

    Add config.option
    src: RefineIt: Support: option.php: We added the reference file to enable addinng some basic configuration for individual plugin mdoule.

---
+++ src/RefineIt/config/option.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Place module specific required wordpress options in this array as it is shown in example below.
+ *
+ * Please do NOT prefix options in this table. When you actually need to get real WP option name concatenate names from this array with one
+ * of prefixes saved in config/prefix.php.
+ * 
+ */
+return [
+
+	/**
+	 * Example record:
+	 *
+	 * '<option-name>' => [
+	 * 		'value'		=> '',
+	 * 		'autoload'	=> 'yes|no'
+	 * 	]
+	 * 
+	 */
+];
